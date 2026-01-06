d82e41fa6c7f2ad17642ef4567bfd964fd072429
Hadjoudj Abdelghafour <ahmedhadjoudj303@gmail.com>
2026-01-06 22:30:26 +0100

    ios

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -22,19 +22,17 @@ const storeRefreshToken = async (adminId, refreshToken) => {
 };
 
 const setCookies = (res, accessToken, refreshToken) => {
-  const isProd = process.env.NODE_ENV === "production";
-
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure: isProd,
-    sameSite: isProd ? "None" : "Lax",
+    secure: true,
+    sameSite: "None",
     maxAge: 15 * 60 * 1000,
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: isProd,
-    sameSite: isProd ? "None" : "Lax",
+    secure: true,
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
