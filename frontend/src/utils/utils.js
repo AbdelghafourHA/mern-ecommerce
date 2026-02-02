2739c5d9a5432f9f89bfcce66cbfb73003fcfa0a
Hadjoudj Abdelghafour <ahmedhadjoudj303@gmail.com>
2026-02-02 19:32:55 +0100

    hero

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -3,7 +3,7 @@ import Cosmetic from "../assets/cosmètiques.JPG";
 import Gift from "../assets/cadeaux.JPG";
 import hero01 from "../assets/hero01.JPG";
 import hero02 from "../assets/hero02.JPG";
-import hero03 from "../assets/hero03.JPG";
+import hero03 from "../assets/hero03.PNG";
 import hero04 from "../assets/hero04.JPG";
 
 export { Perfum, Cosmetic, Gift, hero01, hero02, hero03, hero04 };
